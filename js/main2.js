ad69b8f30891bb50be905321da561c3c90f62593
SaintJo <panchenkovanya@gmail.com>
2023-04-11 09:16:12 +0300

    create add function

--- js/main2.js
+++ js/main2.js
@@ -4,8 +4,10 @@ let num2 = 4;
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
+
+let sumEl = document.getElementById("sum-el");
 function add () {
-	 
+	sumEl.textContent = num1 + num2;
 }
 
 function subtract () {
