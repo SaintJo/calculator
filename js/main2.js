66eeecaefa9dfa0b59ae607f5f09e3ff4284ee50
SaintJo <panchenkovanya@gmail.com>
2023-04-12 07:58:44 +0300

    add subtract function

--- js/main2.js
+++ js/main2.js
@@ -7,11 +7,14 @@ document.getElementById("num2-el").textContent = num2;
 
 let sumEl = document.getElementById("sum-el");
 function add () {
-	sumEl.textContent = num1 + num2;
+	let result = num1 + num2;
+	sumEl.textContent = "Sum: " + result;
 }
 
+let subEl = document.getElementById("sum-el");
 function subtract () {
-	 
+	 let result = num1 - num2;
+	 subEl.textContent = "Sum: " + result;
 }
 
 function divede () {
