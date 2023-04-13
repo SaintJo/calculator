6cb2a257978c6fe12a9ea0723fde3a9472ce667c
SaintJo <panchenkovanya@gmail.com>
2023-04-13 09:01:18 +0300

    add style to button

--- js/main3.js
+++ js/main3.js
@@ -7,20 +7,17 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-// Add Logical Operators
-
-if (sum <= 20) {
-	message = ("Do you want to draw a new card?");
-} else if (sum === 21) {
-	message = ("Welcome! You have got BlackJack!");
-	hasBlackJack = true;
-} else {
-	message = ("You're out of the game!");
-	isAlive = false;
-}
 
 // Create a function startGame()
 
 function startGame () {
-	
+	if (sum <= 20) {
+		message = ("Do you want to draw a new card?");
+	} else if (sum === 21) {
+		message = ("Welcome! You have got BlackJack!");
+		hasBlackJack = true;
+	} else {
+		message = ("You're out of the game!");
+		isAlive = false;
+	}
 }
