54187d4c7f0e9bb7b4186bdda558b68524e6222e
SaintJo <panchenkovanya@gmail.com>
2023-04-13 09:30:50 +0300

    create variable cardsEl

--- js/main3.js
+++ js/main3.js
@@ -1,18 +1,20 @@
 // Create two variabales, firstCard and secondCard.
 
-let firstCard = 6;
-let secondCard = 2;
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEL = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 
 // Create a function startGame()
 
 function startGame () {
+	sumEL.textContent ="Sum: " + sum;
 	if (sum <= 20) {
 		message = ("Do you want to draw a new card?");
 	} else if (sum === 21) {
